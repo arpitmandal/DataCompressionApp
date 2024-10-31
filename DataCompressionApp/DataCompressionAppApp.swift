0ef7259ac4e99602b35ab66123b6eae1fc8b0ce7
Arpit Mandal <workforarpit@gmail.com>
2024-10-30 21:55:10 -0400

    Initial commit

---
+++ DataCompressionApp/DataCompressionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataCompressionAppApp.swift
+//  DataCompressionApp
+//
+//  Created by Arpit Mandal on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct DataCompressionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DataCompressionAppTests/DataCompressionAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  DataCompressionAppTests.swift
+//  DataCompressionAppTests
+//
+//  Created by Arpit Mandal on 2024-10-30.
+//
+
+import Testing
+
+struct DataCompressionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
